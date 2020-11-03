8dd2ddbadee6e6a82b5d5d21cf938f8f58ab7401
Steffi Keene <steffikeene@Steffis-MacBook-Pro-2.local>
2020-11-03 10:05:01 -0500

    grammar changes

--- src/egoism.js
+++ src/egoism.js
@@ -8,7 +8,7 @@ class Egoism extends React.Component {
 
           <h1>Egoism</h1>
 
-          <h3>Egoism claims that each person has but one ultimate aim: his/her own welfare. Egoism claims that a person is morally ought to perform some action if and only if doing it is in that individual's maximum self interset.</h3>
+          <h3>Egoism claims that each person has but one ultimate goal: his/her own welfare. Egoism claims that a person is morally obligated to perform some action if and only if doing that action is in that individual's maximum self interest.</h3>
 
           <button onClick={() => this.props.next()}>Utilitarianism</button>
 
--- src/intro.js
+++ src/intro.js
@@ -8,7 +8,7 @@ class Intro extends React.Component {
 
           <h1>Welcome!</h1>
 
-          <h3>For our project, we created a guided scenario game designed to inform users about the ethical frameworks of Egoism and Utilititarianism. It also gauges a user's response and informs them whether they lean more towards Egoism or Utilitarianism depending on certain contexts. We reccomend spending 5 minutes of reading and thinking before answering each question.</h3>
+          <h3>For our project, we created a guided scenario game designed to inform users about the ethical frameworks of Egoism and Utilititarianism. It also gauges a user's response and informs them whether they lean more towards Egoism or Utilitarianism depending on certain contexts. We recommend spending 5 minutes of reading and thinking before answering each question.</h3>
           <h3>Click the button to learn a bit more about these Ethical Frameworks before the game begins!</h3>
 
           <button onClick={() => this.props.next()}>Egoism</button>
